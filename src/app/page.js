3aa068528164ab9aa76e2b751f9d910a5557fe16
Lidor Ashush <lidor12456@gmail.com>
2023-09-19 01:42:41 +0300

    refactoring events arr - set data from child component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
@@ -11,13 +11,30 @@ export default function Home() {
   const localizer = momentLocalizer(moment);
 
   const [events, setEvents] = useState([]);
+  useEffect(() => {
+    const manipulateEventsForCalendar = (events) => {
+      let eventsForCalendar = [];
+      events.map((event) => {
+        eventsForCalendar.push({
+          title: "mazal tov",
+          start: new Date(event.year, event.month - 1, event.day, 10, 0),
+          end: new Date(event.year, event.month - 1, event.day, 12, 0),
+        });
+      });
+      console.log(eventsForCalendar);
+      setEvents(eventsForCalendar);
+    };
+    manipulateEventsForCalendar(events);
+  }, []);
+
+  console.log(events);
   // [
-  //   {
-  //     title: "Event 1",
-  //     start: new Date(2023, 7, 31, 10, 0),
-  //     end: new Date(2023, 7, 31, 12, 0),
-  //     imageUrl: "https://www.w3schools.com/images/lamp.jpg",
-  //   },
+  // {
+  //   title: "Event 1",
+  //   start: new Date(2023, 7, 31, 10, 0),
+  //   end: new Date(2023, 7, 31, 12, 0),
+  //   imageUrl: "https://www.w3schools.com/images/lamp.jpg",
+  // },
   //   {
   //     title: "Lidor",
   //     start: new Date(PERSONS[0].year, PERSONS[0].month, PERSONS[0].day, 10, 0),
