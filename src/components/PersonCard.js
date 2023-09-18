6d1d219231a2196b9b2911533bb9dc05323cabe8
Lidor Ashush <lidor12456@gmail.com>
2023-09-18 23:57:03 +0300

    refactoring events state and components

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,9 +1,8 @@
-"use client";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-export default function PersonCard({ person }) {
-  const [personData, setPersonData] = useState("");
+function PersonCard({ person, events, setEvents }) {
+  const [personData, setPersonData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,51 +10,52 @@ export default function PersonCard({ person }) {
         const { data } = await axios.get(
           `https://www.hebcal.com/yahrzeit?cfg=json&n1=${person.name}&t1=Birthday&d1=${person.day}&m1=${person.month}&y1=${person.year}&hebdate=on&hdp=1&years=6`
         );
-        console.log(data);
+
         setPersonData(data);
       } catch (e) {
         console.log(e.message);
       }
     };
+
     fetchData();
   }, []);
-  let events = [];
-  const createEventFromPersonDate = (personData) => {
-    personData.items.map((person) => {
-      events.push({
-        name: person.name,
-        year: person.date.slice(0, 4),
-        month: person.date.slice(5, 7),
-        day: person.date.slice(8, 10),
-      });
-    });
-    console.log(events);
-  };
-  if (personData) {
-    createEventFromPersonDate(personData);
-  }
+
+  useEffect(() => {
+    if (personData) {
+      const eventsFromPerson = personData.items.map((item, i) => ({
+        name: item.name,
+        year: item.date.slice(0, 4),
+        month: item.date.slice(5, 7),
+        day: item.date.slice(8, 10),
+      }));
+
+      // Update the events state by merging new events
+      setEvents((prevEvents) => [...prevEvents, ...eventsFromPerson]);
+    }
+  }, [personData, setEvents]);
+
   return (
-    <>
-      <div>
-        {personData && console.log(personData.items)}
+    <div>
+      {personData && (
         <div>
           <p>{person.name}</p>
         </div>
-        <div>
-          <div class="flex flex-row flex-row gap-3">
-            {personData &&
-              personData.items.map((item) => (
-                <div class="px-1 py-1 w-1/2 rounded-lg bg-[#e5e7eb] shadow-lg">
-                  <p>{item.date}</p>
-                  <p>{item.date.slice(0, 4)}</p>
-                  <p>{item.date.slice(5, 7)}</p>
-                  <p>{item.date.slice(8, 10)}</p>
-                  <p>{`${item.heDateParts.d} ${item.heDateParts.m} ${item.heDateParts.y}`}</p>
-                </div>
-              ))}
-          </div>
-        </div>
+      )}
+      <div className="flex flex-row gap-3">
+        {personData &&
+          personData.items.map((item) => (
+            <div
+              key={item.date}
+              className="px-1 py-1 w-1/2 rounded-lg bg-[#e5e7eb] shadow-lg"
+            >
+              {console.log(events)}
+              <p>{item.date}</p>
+              <p>{`${item.heDateParts.d} ${item.heDateParts.m} ${item.heDateParts.y}`}</p>
+            </div>
+          ))}
       </div>
-    </>
+    </div>
   );
 }
+
+export default React.memo(PersonCard);
