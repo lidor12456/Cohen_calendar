99b639d859e8f77310d00b5d36e1b472cda1f303
Lidor Ashush <lidor12456@gmail.com>
2024-01-09 13:51:20 +0200

    split files

--- src/app/db/dbConfig.js
+++ src/app/db/dbConfig.js
@@ -1,45 +1,9 @@
-// const mongoose = require("mongoose");
-// const URL =
-//   "mongodb+srv://lidor_ashush:knc7jpMsAK4B@cluster0.nekqmgn.mongodb.net/";
-// async function connect() {
-//   try {
-//     await mongoose.connect(URL);
-//     console.log("connect to db");
-//   } catch (error) {
-//     throw new Error("Connection failed!");
-//   }
-// }
+import { MongoClient } from "mongodb";
 
-// module.exports = connect;
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://lidor_ashush:knc7jpMsAK4B@cluster0.nekqmgn.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("cohen_calendar").command({ ping: 1 });
-    console.log(client);
-    // await client.db("cohen_calendar").collection("persons").find({});
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+export default function connectToMongoDb() {
+  const client = new MongoClient(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  return client;
 }
-module.exports = { run, client };
-// run().catch(console.dir);
