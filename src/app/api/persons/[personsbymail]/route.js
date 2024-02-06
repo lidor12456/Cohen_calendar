159682b103ec87397cd2ce7560623919f95a0861
Lidor Ashush <lidor12456@gmail.com>
2024-02-06 13:02:05 +0200

    add dynamic route to find persons by mail

---
+++ src/app/api/persons/[personsbymail]/route.js
@@ -0,0 +1,27 @@
+import connectToMongoDb from "@/app/db/dbConfig";
+import { NextResponse } from "next/server";
+
+export async function GET(request, context) {
+  const userMail = context.params.personsbymail;
+  const client = await connectToMongoDb();
+
+  try {
+    await client.connect();
+    const db = client.db("cohen_calendar");
+    const personsByMail = await db
+      .collection("persons")
+      .find({ createdBy: userMail })
+      .toArray();
+    if (personsByMail.length > 0) {
+      return NextResponse.json(personsByMail);
+    } else {
+      return NextResponse.json({
+        error: "Unable to find persons by this mail",
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: "Unable to connect" });
+  } finally {
+    await client.close();
+  }
+}
--- src/app/api/persons/route.js
+++ src/app/api/persons/route.js
@@ -1,3 +1,4 @@
+// get all persons
 import { NextRequest, NextResponse } from "next/server";
 import connectToMongoDb from "../../db/dbConfig";
 
