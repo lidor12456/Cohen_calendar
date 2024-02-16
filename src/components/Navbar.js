473728f03adcb872ccf2373e166f016e8cba72b2
Lidor Ashush <lidor12456@gmail.com>
2024-02-16 12:26:28 +0200

    responsive nav continue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,16 +14,36 @@ const Navbar = () => {
   const status = useSession();
   return (
     <nav className="bg-[#e5e7eb] shadow-lg p-4">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-3">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-white text-2xl font-bold">
           Calendar
         </Link>
+        <button className="md:hidden" onClick={toggleMenu}>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
         {status.status === "authenticated" ? (
           <span className="text-gray-600">{`Hello ${status.data?.user?.name}`}</span>
         ) : (
           <span className="text-gray-600">Please Sign In</span>
         )}
-        <ul className="flex flex-col md:flex-row space-y-2 md:space-y-1 md:space-x-4">
+        <ul
+          className={`md:flex flex-wrap items-center space-x-2 ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
           <li>
             <Link
               href="/personsList"
@@ -44,7 +64,7 @@ const Navbar = () => {
             {status.status === "authenticated" ? (
               <button
                 onClick={() => signOut()}
-                className="bg-blue-500 text-white py-2 px-4 rounded-lg mr-4 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+                className="bg-red-500 text-white py-2 px-4 rounded-lg mr-4 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
               >
                 Sign Out
               </button>
