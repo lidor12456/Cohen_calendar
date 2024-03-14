6f41993c9a692501ea1ccc3daeb140a0a96259ae
Lidor Ashush <lidor12456@gmail.com>
2024-03-14 12:15:39 +0200

    refactor dates in form

---
+++ src/app/addnew/components/DateField.js
@@ -0,0 +1,55 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+function DateField({ isDisabled }) {
+  return (
+    <div className="mb-4 flex">
+      <div className="mr-4 w-1/3">
+        <label htmlFor="day" className="block text-gray-600 font-semibold mb-2">
+          Day:
+        </label>
+        <Field
+          disabled={isDisabled}
+          type="number"
+          id="day"
+          name="day"
+          className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+        />
+        <ErrorMessage name="day" component="div" className="text-red-500" />
+      </div>
+
+      <div className="mr-4 w-1/3">
+        <label
+          htmlFor="month"
+          className="block text-gray-600 font-semibold mb-2"
+        >
+          Month:
+        </label>
+        <Field
+          type="number"
+          id="month"
+          name="month"
+          className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+        />
+        <ErrorMessage name="month" component="div" className="text-red-500" />
+      </div>
+
+      <div className="w-1/3">
+        <label
+          htmlFor="year"
+          className="block text-gray-600 font-semibold mb-2"
+        >
+          Year:
+        </label>
+        <Field
+          type="number"
+          id="year"
+          name="year"
+          className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+        />
+        <ErrorMessage name="year" component="div" className="text-red-500" />
+      </div>
+    </div>
+  );
+}
+
+export default DateField;
--- src/app/addnew/page.js
+++ src/app/addnew/page.js
@@ -1,9 +1,11 @@
 "use client";
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { postData } from "../utils";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import DateField from "./components/DateField";
 
 const validationSchema = Yup.object({
   firstName: Yup.string().required("First Name is required"),
@@ -22,7 +24,16 @@ const validationSchema = Yup.object({
     .max(new Date().getFullYear(), "Invalid year"),
 });
 
+const monthsArr = [
+  // { enName: "", heName: "" },
+  "tishrei",
+  "heshvan",
+];
+
 export default function AddNewPersonForm() {
+  const [date1Filled, setDate1Filled] = useState(false);
+  const [date2Filled, setDate2Filled] = useState(false);
+
   const status = useSession();
   const initialValues = {
     firstName: "",
@@ -30,6 +41,7 @@ export default function AddNewPersonForm() {
     day: "",
     month: "",
     year: "",
+    dropdownValue: "",
   };
 
   async function onSubmit(values, { resetForm }) {
@@ -38,7 +50,6 @@ export default function AddNewPersonForm() {
       createdBy: status?.data?.user?.email || "unknown",
     };
     console.log("Form submitted:", dataToSend);
-    // const baseUrl = process.env.PRODUCTION_API_URL || "http://localhost:3000";
     await postData(`/api/persons`, dataToSend);
     resetForm();
   }
@@ -92,7 +103,7 @@ export default function AddNewPersonForm() {
               />
             </div>
 
-            <div className="mb-4 flex">
+            {/* <div className="mb-4 flex">
               <div className="mr-4 w-1/3">
                 <label
                   htmlFor="day"
@@ -101,6 +112,7 @@ export default function AddNewPersonForm() {
                   Day:
                 </label>
                 <Field
+                  // disabled
                   type="number"
                   id="day"
                   name="day"
@@ -153,13 +165,104 @@ export default function AddNewPersonForm() {
                 />
               </div>
             </div>
+            <div className="mb-4 flex">
+              <div className="mr-4 w-1/3">
+                <label
+                  htmlFor="day"
+                  className="block text-gray-600 font-semibold mb-2"
+                >
+                  Day:
+                </label>
+                <Field
+                  // disabled
+                  type="number"
+                  id="day"
+                  name="day"
+                  className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+                <ErrorMessage
+                  name="day"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
 
+              <div className="mr-4 w-1/3">
+                <label
+                  htmlFor="month"
+                  className="block text-gray-600 font-semibold mb-2"
+                >
+                  Month:
+                </label>
+                <Field
+                  type="number"
+                  id="month"
+                  name="month"
+                  className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+                <ErrorMessage
+                  name="month"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
+
+              <div className="w-1/3">
+                <label
+                  htmlFor="year"
+                  className="block text-gray-600 font-semibold mb-2"
+                >
+                  Year:
+                </label>
+                <Field
+                  type="number"
+                  id="year"
+                  name="year"
+                  className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+                <ErrorMessage
+                  name="year"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
+            </div> */}
+
+            <div className="ml-[15rem] my-3 [direction:rtl] text-base">
+              אני יודע תאריך עברי :
+            </div>
+            <DateField isDisabled={true} />
+            <div className="ml-[15rem] my-3 [direction:rtl] text-base">
+              אני יודע תאריך לעוזי :
+            </div>
+            <DateField />
             <button
               type="submit"
               className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
             >
               Submit
             </button>
+            <div>
+              <label htmlFor="dropdownValue">Choose an option:</label>
+              <Field as="select" id="dropdownValue" name="dropdownValue">
+                {monthsArr.map((month) => {
+                  return (
+                    <option value={month} key={month}>
+                      {month}
+                    </option>
+                  );
+                })}
+                {/* <option value="">Select...</option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option> */}
+              </Field>
+              <ErrorMessage
+                name="dropdownValue"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
           </Form>
         </Formik>
       </ProtectedRoute>
